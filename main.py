cbadecb6500775efa3e3f7225cb7fc4d7f69da53
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-26 17:10:56 +0700

    implement template inheritance

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world</p>"
+    return render_template('index.html')
 
 
-@app.route("/test")
-def open_test():
-    return render_template('hello.html')
+if __name__ == "__main__":
+    app.run(debug=True)
