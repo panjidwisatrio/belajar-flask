9a22fa00f4b3afdf1ced16bd2754227290c2c92e
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-30 14:47:48 +0700

    add update task fun

--- app.py
+++ app.py
@@ -51,5 +51,20 @@ def delete_task(id):
         print("Unexpected error. Details: {}".format(x))
 
 
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as x:
+            print("Unexpected error. Details: {}".format(x))
+    else:
+        return render_template('update.html', task=task)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
