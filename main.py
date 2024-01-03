6d711de9edab618ace025da54f048b845cb26c51
Panji Dwi Satrio <panjidwisatrio@student.telkomuniversity.ac.id>
2024-01-03 13:51:00 +0700

    add some endpoint crud and upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 base_dir = '/home/site/wwwroot'
 
@@ -19,6 +19,57 @@ async def create_folder_file():
         with open(base_dir + '/data/test.txt', 'r') as f:
             data = f.read()
         return {'status': 'success', 'data': data}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+# Endpoint to read file
+@app.get("/read_file")
+async def read_file():
+    try:
+        data = None
+        with open(base_dir + '/data/test.txt', 'r') as f:
+            data = f.read()
+        return {'status': 'success', 'data': data}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+# Endpoint to delete file
+@app.get("/delete_file")
+async def delete_file():
+    try:
+        os.remove(base_dir + '/data/test.txt')
+        return {'status': 'success'}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+    
+# Endpoint to delete folder
+@app.get("/delete_folder")
+async def delete_folder():
+    try:
+        os.rmdir(base_dir + '/data')
+        return {'status': 'success'}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+# Endpoint to get all files in folder
+@app.get("/get_files")
+async def get_files():
+    try:
+        files = os.listdir(base_dir)
+        return {'status': 'success', 'data': files}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+# Endpoint to upload file
+@app.post("/upload_file")
+async def upload_file(
+    file: UploadFile = File(...),
+):
+    try:
+        contents = await file.read()
+        with open(base_dir + '/data/' + file.filename, 'wb') as f:
+            f.write(contents)
+        return {'status': 'success'}
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
 
