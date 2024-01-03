801f113a4dedb9d2ec55f68ae001949e3b795b83
Samuel Rusli <samuel.rusli@global.ntt>
2024-01-03 11:26:02 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import uvicorn
+from fastapi import FastAPI
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = FastAPI(title="Automate Preventive Maintenance API", version="1.0", )
+
+# Endpoint to create folder and file
+@app.get("/create_folder_file")
+async def create_folder_file():
+    try:
+        os.mkdir(base_dir + '/data')
+        with open(base_dir + '/data/test.txt', 'w') as f:
+            f.write('Hello World!')
+        return {'status': 'success'}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
